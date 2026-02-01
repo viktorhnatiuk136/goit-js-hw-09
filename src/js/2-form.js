4e13c7c8ca3b65be079d4882dd717630319b8cc3
viktor hnatiuk <viktorgnatiuk136@gmail.com>
2026-02-01 14:06:56 +0200

    did tasc

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,42 @@
-console.log("Form");
+const formData = {
+    email: "",
+    message: ""
+};
+
+const form = document.querySelector(".feedback-form");
+
+
+const saveDataString = localStorage.getItem("feedback-form-state");
+const saveData = saveDataString ? JSON.parse(saveDataString) : formData;
+
+form.elements.email.value = saveData.email;
+form.elements.message.value = saveData.message;
+formData.email = saveData.email;
+formData.message = saveData.message;
+
+form.addEventListener("input", (event) => {
+    
+    const fieldName = event.target.name;
+    const fieldValue = event.target.value;
+    
+    if (fieldName === "email" || fieldName === "message") {
+        formData[fieldName] = fieldValue;
+        localStorage.setItem("feedback-form-state", JSON.stringify(formData))
+    }
+});
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (formData.email === "" || formData.message === "") {
+        alert("Fill please all fields");
+        return;
+    } console.log(formData);
+    localStorage.removeItem("feedback-form-state");
+    formData.email = "";
+    formData.message = "";
+    form.reset();
+});
+
+
+
+
